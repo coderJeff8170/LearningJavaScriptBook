af4f485131b834bba766f0db912ed8fb2513fd6d
Jeff Andrews <jeff@paintedbyjeff.com>
2019-03-27 08:40:22 -0400

    home 0840 032819

--- scope.js
+++ scope.js
@@ -45,11 +45,66 @@
 //basically at the start of your program
 //anything you declare here is available to all other scopes in your program
 
+// let name = "Irena";//global
+// let age = 25;//global
 
+// function greet() {
+//     console.log(`Hello,  ${name}`);
+// }
+// function getBirthYear() {
+//     return new Date().getFullYear() - age;
+// }
+//BY THE WAY - THIS WONT RUN BECAUSE THE FUNTIONS HAVENT BEEN CALLED
+//^^this is a problem because you may run into problems with such generic
+//variable names as 'name' and 'age' at the global level
+// a better approach is to put user information in a single object:
 
-//BLOCK
+// let user = {
+//     name = "Irena",
+//     age = 25,
+// };
+//AUTHOR MADE A MISTAKE - this will generate an "invalid property initializer" error
+//because when declaring an object a property must have a colon between it and the value.
 
+// let user = {
+//     name: "Irena",
+//     age: 25,
+// };//unfortunately, the functions themselves are still dependent on the global scope
+// function greet() {
+//     console.log(`Hello, ${user.name}`);
+// }
+// function getBirthYear() {
+//     user.age = new Date().getFullYear();
+// }
+// //^this is pretty cool - author had this vvvv not sure that even does anything
+// //     return new Date().getFullYear() - user.age;
+// greet();
+// getBirthYear();
+// console.log(`You are ${user.age} years old!`);
 
+let user = {
+    name: "Irena",
+    age: 25,
+};
+function greet(user) {
+    console.log(`Hello, ${this.name}`);
+}
+function getBirthYear(user) {
+    console.log(this.age);
+    return new Date().getFullYear() - this.age;
+}
 
-//FUNCTION
+greet();
+greet(user);
+// greet(Irena);
+console.log(getBirthYear());
+console.log(`You are ${user.age} years old!`);
 
+//ok so the authors shit still doesn't work - it throws an error until I use `this`
+//I guess I'm finally getting that
+//It works now, but I'm getting 
+//Hello, undefined
+//Hello, undefined
+//undefined
+//NaN
+//You are 25 years old!
---
+++ scope1.js
@@ -0,0 +1,45 @@
+//BLOCK
+
+//let and const declare identifiers in block scope
+//a block is a list of statements within curly braces
+//so block scope is limited to within those braces
+
+// console.log('before block');
+// {
+//     console.log('inside block');
+//     const x = 3;
+//     console.log(x);
+// }
+// console.log(`outside block: x = ${x}`); Will throw error because x is in block scope
+//vvjust a test
+// y = "Jeff";
+// console.log(`outside block: y = ${y}`);
+//^^there's little practical use for standalone blocks other than to test things
+
+//VARIABLE MASKING - two variable with same name can be in scope at same time, but
+//while inner one is being used, the outer is 'masked' and cannot be accessed 
+//until that inner scope is exited:
+{
+    //outer block
+    let x = { color: "blue" };
+    let y = x; //y and x now refer to same object
+    let z = 3;
+    {
+        //inner block
+        let x = 5; //outer x is now masked
+        console.log(x); //logs 5
+        console.log(y.color); //logs "blue"; object pointed to by y(and x in outer scope)
+                            //is still in scope
+        y.color = "red";//modifies original object pointed to by y!
+        console.log(y.color);
+        console.log(z); //logs 3; z was never masked
+    }
+    console.log(x.color); //logs "red"; object modified in inner scope by y!
+    console.log(y.color); //logs "red"; x and y point to same object in outer scope
+    console.log(x);
+    console.log(y);
+    console.log(z);
+
+}
+//FUNCTION
+
