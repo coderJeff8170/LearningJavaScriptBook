8b924857b3b29c2d5a2c1505c18d10220346c832
Jeff Andrews <jeff@paintedbyjeff.com>
2019-04-01 06:40:15 -0400

    home 0639 040119

--- arrays.js
+++ arrays.js
@@ -10,11 +10,33 @@
 
 const arr1 = ["b", "c", "d"];
 
-console.log(arr1.push("e")); //4
+console.log(arr1.push("e")); //4 (length of array returned, e added to end)
 console.log(arr1);          // ["b", "c", "d", "e"]
-console.log(arr1.pop());    //e
+console.log(arr1.pop());    //e (e returned, e removed from end)
 console.log(arr1);          // ["b", "c", "d"]
-console.log(arr1.unshift("a")); //4
+console.log(arr1.unshift("a")); //4 (length of array returned, a added to beginning)
 console.log(arr1);          // ["a", "b", "c", "d"]
-console.log(arr1.shift());  //a
+console.log(arr1.shift());  //a (a returned, a removed from beginning)
 console.log(arr1);          // ["b", "c", "d"]
+
+//the concat() method adds multiple elements to the array and returns a copy
+
+const arr2 = [1, 2, 3];
+
+console.log(arr2.concat(4, 5, 6)); //returns [1, 2, 3, 4, 5, 6] arr2 unmodified
+console.log(arr2.concat([4, 5, 6])); //returns [1, 2, 3, 4, 5, 6] arr2 unmodified
+console.log(arr2.concat([4, 5], 6)); //returns [1, 2, 3, 4, 5, 6] arr2 unmodified
+console.log(arr2.concat(4, [5, 6])); //returns [1, 2, 3, 4, 5, 6] arr2 unmodified (another author error)
+
+//slice allows you to get a subarray from an array, takes two arguments,
+//which are the beginning and ending indexes. First is inclusive, the second is not.
+//if you omit the second argument, it returns everything to the end of the string.
+
+const arr3 = [1, 2, 3, 4, 5];
+
+console.log();
+console.log();
+console.log();
+
+console.log();
+
