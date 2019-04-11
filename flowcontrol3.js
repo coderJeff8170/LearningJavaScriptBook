48c2238d10016f1b2ac5c566d8b090e7b64f7b24
Jeff Andrews <jeff@paintedbyjeff.com>
2019-04-11 09:42:42 -0400

    home 0942 041119

---
+++ flowcontrol3.js
@@ -0,0 +1,15 @@
+//switch statements 
+//allow you to take multiple paths based upon one statement
+
+switch (expression) {
+    case value1: //executed when result of expression matches value1;
+    break;
+    case value1: //executed when result of expression matches value1;
+    break;
+    ...
+    case valueN: //executed when result of expression matches value1;
+    break;
+    default: //executed when result of expression matches value1;
+    break;
+}
+
---
+++ piratefarkle.js
@@ -0,0 +1,29 @@
+//dice
+    //numberfaces, faces
+
+//roll
+    //5 dice
+    //keep rolling until better score or crap out
+
+//tally
+    //crap out or add winnings to score
+
+//round
+    //keep playing until one player reaches max score
+
+//players
+    //up to five
+
+//scoreloop
+
+//faces
+    //1 - 5, and a jolly roger, so values and images
+
+//interface
+    //html interface, styled by css
+    //number of players
+    //rules
+    //start button
+    //five positions one for each die
+    //stay or roll buttons
+    //main roll button
